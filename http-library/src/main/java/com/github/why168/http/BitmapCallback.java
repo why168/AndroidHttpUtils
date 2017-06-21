d42d0b5aa8987992175bcfe0707a7357d39f1af1
Edwin Wu <edwin_wu05@126.com>
2017-06-22 00:24:18 +0800

    README

--- http-library/src/main/java/com/github/why168/http/BitmapCallback.java
+++ http-library/src/main/java/com/github/why168/http/BitmapCallback.java
@@ -3,7 +3,6 @@ package com.github.why168.http;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -19,20 +18,11 @@ public abstract class BitmapCallback extends Callback<Bitmap> {
     @Override
     public Bitmap parseNetworkResponse(Response response, AtomicBoolean isCancelled) throws HttpException, IOException {
         InputStream is = response.getInputStream();
-        ByteArrayOutputStream out = new ByteArrayOutputStream(2048);
-        byte[] buffer = new byte[2048];
-        int len = 0;
-        while ((len = is.read(buffer)) != -1) {
-            if (isCancelled.get())
-                throw new HttpException(HttpException.ErrorType.CANCEL, "http cancel");
-
-            out.write(buffer, 0, len);
-        }
-        out.flush();
+        if (isCancelled.get())
+            throw new HttpException(HttpException.ErrorType.CANCEL, "http cancel");
+        Bitmap bitmap = BitmapFactory.decodeStream(is);
         is.close();
-        out.close();
-        byte[] body = out.toByteArray();
-        return BitmapFactory.decodeByteArray(body, 0, body.length);
+        return bitmap;
     }
 
 }
